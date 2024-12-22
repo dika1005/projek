9dd2858d80d5089ec7588c90507fc54b8c8c51a5
Albar19 <muhamadhafizhalbar@gmail.com>
2024-12-22 18:15:18 +0700

    new2

---
+++ src/main/java/kelompok6/projek/Projek.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package kelompok6.projek;
+
+import kelompok6.form.menu;
+
+
+/**
+ *
+ * @author bayui
+ */
+public class Projek {
+
+    public static void main(String[] args) {
+        menu utama = new menu();
+        utama.setVisible(true);
+    }
+}
