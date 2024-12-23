a5312f993b0f2c69d0c88b1c017eb5af97437189
by.ulimitid <bayuimantoro4@gmail.com>
2024-12-23 14:16:07 +0700

    fix: rename class 'dartar' to 'Daftar' for consistency and clarity

--- src/main/java/kelompok6/form/Daftar.java
+++ src/main/java/kelompok6/form/Daftar.java
@@ -8,12 +8,12 @@ package kelompok6.form;
  *
  * @author LOQA
  */
-public class dartar extends javax.swing.JFrame {
+public class Daftar extends javax.swing.JFrame {
 
     /**
      * Creates new form register
      */
-    public dartar() {
+    public Daftar() {
         initComponents();
     }
 
@@ -157,13 +157,13 @@ public class dartar extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(dartar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Daftar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(dartar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Daftar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(dartar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Daftar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(dartar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Daftar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
         //</editor-fold>
@@ -171,7 +171,7 @@ public class dartar extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new dartar().setVisible(true);
+                new Daftar().setVisible(true);
             }
         });
     }
